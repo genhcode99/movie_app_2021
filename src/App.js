3a3a3b010598401c2e78e8317949e068d18e299b
JeUpark <genhcode@gmail.com>
2021-05-28 14:43:15 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from "react";
 // import PropTypes from "prop-types"
 
 class App extends React.Component{
-  state = {
-    count: 0
-  };
-
   add = () => {
     this.setState( current => ({ count: current.count +1 }));
   };
-
   minus = () => {
     this.setState( current => ({ count: current.count -1 }));
   };
-
+  
   render(){
+  
     return (
       <div>
       <h1>The number is :{this.state.count}</h1>
@@ -22,6 +18,7 @@ class App extends React.Component{
       <button onClick={this.minus}>Minus</button>
       </div>
     )
+    
   }
 }
 
