e9b66884f63fb0c34ff3c8a1a32ec035751a20c4
JeUpark <genhcode@gmail.com>
2021-05-28 12:13:28 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 
+
+const foodLike = [
+  {id:1, name: "kimchi"},
+  {id:2, name: "samgyeopsal"},
+  {id:3, name: "Bibibap"},
+  {id:4, name: "salt"}
+]
+
+
 function Food({name}){
   return (
     <h1>I like {name}</h1>
   )
 }
 
-const foodLike = [{name: "kimchi"},{name: "samgyeopsal"},{name: "Bibibap"},{name: "salt"}]
-
 
 function App() {
   return(
     <div>
-      {foodLike.map( dish => <Food name={dish.name}/>)}
+      {foodLike.map( dish => <Food key={dish.id} name={dish.name}/>)}
     </div>
   )
 }
+
 
 export default App;
