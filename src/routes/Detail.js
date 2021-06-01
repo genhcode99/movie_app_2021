4cf3af7dea3885ae7291f0aedfc7d93948ecfb7e
JeUpark <genhcode@gmail.com>
2021-06-01 10:35:44 +0900

    #6.4 Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props)
-  return <span>hello</span>
+class Detail extends React.Component{
+  componentDidMount(){
+    const { location, history } = this.props
+    if(location.state === undefined){
+      history.push("/");
+    }
+  }
+  render(){
+    const { state } = this.props.location
+    if(state){
+      return (
+      <span> {state.title}</span>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
