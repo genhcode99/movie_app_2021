e5ad7f13ccb1f9fdafa5bce0eef0758bd1c99efc
JeUpark <genhcode@gmail.com>
2021-06-01 10:50:18 +0900

    #6.4 Redirecting , 코드정리

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,19 @@
 import React from "react";
 
-class Detail extends React.Component{
-  componentDidMount(){
-    const { location, history } = this.props
-    if(location.state === undefined){
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
       history.push("/");
     }
   }
-  render(){
-    const { state } = this.props.location
-    if(state){
-      return (
-      <span> {state.title}</span>
-      );
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
     } else {
       return null;
     }
   }
 }
-
 export default Detail;
